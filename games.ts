775ebbf521aa3c96379812482b18225f7e883c92
Jazadfir <lordnelson010@gmail.com>
2025-05-12 21:44:46 +0300

    Add files via upload

---
+++ games.ts
@@ -0,0 +1,148 @@
+[
+    {
+      "title": "Counter-Strike: Global Offensive",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg",
+      "shortDesc": "Rekabetçi FPS deneyimi.",
+      "description": "Dünyanın dört bir yanından oyuncularla ekip tabanlı mücadeleye katılın.",
+      "genre": "FPS",
+      "release": "2012-08-21",
+      "price": "Free to Play",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/PCpTpyPnM9k"
+    },
+    {
+      "title": "Dota 2",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/570/header.jpg",
+      "shortDesc": "Stratejik MOBA aksiyon.",
+      "description": "Kahramanlarınızı seçin ve rakip ekibi yok etmek için strateji oluşturun.",
+      "genre": "MOBA",
+      "release": "2013-07-09",
+      "price": "Free to Play",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/r2MUK8o7gTI"
+    },
+    {
+      "title": "PLAYERUNKNOWN'S BATTLEGROUNDS",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/578080/header.jpg",
+      "shortDesc": "Hayatta kalma Battle Royale.",
+      "description": "100 oyuncu arasında son kişi olmak için mücadele edin.",
+      "genre": "Battle Royale",
+      "release": "2017-12-20",
+      "price": "₺49.99",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/9lHgU9wQ35I"
+    },
+    {
+      "title": "Apex Legends",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/1172470/header.jpg",
+      "shortDesc": "Takım tabanlı Battle Royale.",
+      "description": "Karakter seçin, takımınızı kurun ve zafer için savaşın.",
+      "genre": "Battle Royale",
+      "release": "2019-02-04",
+      "price": "Free to Play",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/innmNEWjWVI"
+    },
+    {
+      "title": "Grand Theft Auto V",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/271590/header.jpg",
+      "shortDesc": "Açık dünya aksiyon.",
+      "description": "Los Santos sokaklarında suç dünyasında yükselin.",
+      "genre": "Open World",
+      "release": "2013-09-17",
+      "price": "₺299.00",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/QkkoHAzjnUs"
+    },
+    {
+      "title": "Cyberpunk 2077",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/1091500/header.jpg",
+      "shortDesc": "Geleceğin açık dünyası.",
+      "description": "Night City'de bir kahraman yaratın ve destansı hikayeye katılın.",
+      "genre": "RPG",
+      "release": "2020-12-10",
+      "price": "₺249.00",
+      "discount": "%20",
+      "campaign": "Yaz İndirimi",
+      "video": "https://www.youtube.com/embed/LBUG_27E_IN"
+    },
+    {
+      "title": "Elden Ring",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg",
+      "shortDesc": "Açık dünya Souls tarzı.",
+      "description": "Runes topraklarında destansı bir yolculuğa çıkın.",
+      "genre": "Action RPG",
+      "release": "2022-02-25",
+      "price": "₺349.00",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/E3Huy2cdih0"
+    },
+    {
+      "title": "Hades",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/1145360/header.jpg",
+      "shortDesc": "Mitoloji rogue-like.",
+      "description": "Ölüler diyarından kaçmak için savaşın.",
+      "genre": "Rogue-like",
+      "release": "2020-09-17",
+      "price": "₺32.00",
+      "discount": "",
+      "campaign": "Bahar İndirimi",
+      "video": "https://www.youtube.com/embed/591V2QWZbHU"
+    },
+    {
+      "title": "Stardew Valley",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/413150/header.jpg",
+      "shortDesc": "Çiftlik simülasyonu.",
+      "description": "Eski çiftliğinizi restore ederek yeni bir hayata başlayın.",
+      "genre": "Simulation",
+      "release": "2016-02-26",
+      "price": "₺25.00",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/ot7uXNQskhs"
+    },
+    {
+      "title": "Terraria",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/105600/header.jpg",
+      "shortDesc": "Sandbox macera.",
+      "description": "Maden kazın, düşmanları yenin ve kendi dünyanızı inşa edin.",
+      "genre": "Sandbox",
+      "release": "2011-05-16",
+      "price": "₺17.00",
+      "discount": "%50",
+      "campaign": "Hafta Sonu Fırsatı",
+      "video": "https://www.youtube.com/embed/tWn1r7Gps3Y"
+    },
+    {
+      "title": "Warframe",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/230410/header.jpg",
+      "shortDesc": "Görsel açıdan çarpıcı MMO.",
+      "description": "Ninja robotlarıyla düşmanları alt edin ve uzayın derinliklerine yolculuk edin.",
+      "genre": "MMO",
+      "release": "2013-03-25",
+      "price": "Free to Play",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/b1lWZbZyWLM"
+    },
+    {
+      "title": "Rust",
+      "img": "https://cdn.akamai.steamstatic.com/steam/apps/252490/header.jpg",
+      "shortDesc": "Hayatta kalma PvP.",
+      "description": "Açık dünyada kaynak toplayın, üs inşa edin ve diğer oyuncularla etkileşime geçin.",
+      "genre": "Survival",
+      "release": "2018-02-08",
+      "price": "₺99.00",
+      "discount": "",
+      "campaign": "",
+      "video": "https://www.youtube.com/embed/0pOWeKwjgl8"
+    }
+    // ... Kalan oyunları benzer formatta ekleyerek toplam 250'e tamamlayın.
+  ]
+  
